1aa47f9535969248477c4ed312ef7fe5b5d86ce2
maheshsabnis <sabnis_m@hotmail.com>
2021-08-05 09:27:49 +0530

    Add project files.

---
+++ Core_API_ImageUploader/Controllers/FileUploadController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Core_API_ImageUploader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        
+        [HttpPost("file/upload")]
+        public async Task<IActionResult> Upload([FromForm]IFormFile file)
+        {
+            try
+            {
+                
+                if (!CheckFileExtension(file))
+                {
+                    return BadRequest($"The File does not have an extension or it is not image. " +
+                        $"The Expected extension is .jpg/.png/.bmp");
+                }
+
+                if (!CheckFileSize(file))
+                {
+                    return BadRequest($"The size of file is more than 10 mb, " +
+                        $"please make sure that the file size must be less than 10 mb");
+                }
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "Files");
+                 
+                if (file.Length > 0)
+                {
+                   
+                    var postedFileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition)
+                        .FileName.Trim('"');
+                    
+                    
+                    var finalPath = Path.Combine(folder, postedFileName);
+                    using (var fs = new FileStream(finalPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fs);
+                    }
+                    return Ok($"File is uploaded Successfully");
+                }
+                else
+                {
+                    return BadRequest("The File is not received.");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Some Error Occcured while uploading File {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// The file extension must be jpg/bmp/png
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private bool CheckFileExtension(IFormFile file)
+        {
+            string[] extensions = new string[] { "jpg", "bmp", "png" };
+
+            var fileNameExtension = file.FileName.Split(".")[1];
+            if (string.IsNullOrEmpty(fileNameExtension) || !extensions.Contains(fileNameExtension))
+            {
+                return false;
+            }
+           
+            return true;
+        }
+        /// <summary>
+        /// Check the file size, it must be less than 10 mb
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private bool CheckFileSize(IFormFile file)
+        {
+            if (file.Length > 1e+7)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Core_API_ImageUploader/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_API_ImageUploader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddCors(options => {
+                options.AddPolicy("cors", policy => {
+                    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Core_API_ImageUploader", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core_API_ImageUploader v1"));
+            }
+            app.UseCors("cors");
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider
+              (Path.Combine(Directory.GetCurrentDirectory(), @"Files")),
+                RequestPath = new PathString("/Files")
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
